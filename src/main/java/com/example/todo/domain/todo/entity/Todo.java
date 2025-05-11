5b91758d544755e24e38b7548c6d3e648321da70
hyun5085 <ksw9250@naver.com>
2025-05-12 00:31:08 +0900

    일정 삭제 관련 수정

--- src/main/java/com/example/todo/domain/todo/entity/Todo.java
+++ src/main/java/com/example/todo/domain/todo/entity/Todo.java
@@ -1,15 +1,22 @@
 package com.example.todo.domain.todo.entity;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.todo.common.entity.BaseEntity;
+import com.example.todo.domain.comment.entity.Comment;
 import com.example.todo.domain.todo.dto.request.CreateTodoRequestDto;
 import com.example.todo.domain.todo.dto.request.UpdateTodoRequestDto;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 
@@ -31,6 +38,9 @@ public class Todo extends BaseEntity {
 	private String title;
 
 	private String content;
+
+	@OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
+	private List<Comment> comments = new ArrayList<>();
 
 	public Todo(){}
 
